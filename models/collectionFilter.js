c1966537fd7576641a001bc1ea9fd15e72ae34c8
Chahine-Benramoul <48608784+Chahine-Benramoul@users.noreply.github.com>
2023-10-23 23:45:21 -0400

    done

--- models/collectionFilter.js
+++ models/collectionFilter.js
@@ -15,52 +15,45 @@ export default class CollectionFilter {
             // The parameter : prendre le premier param puis faire and le 2 eme ... and 3 eme
             switch (key) {
                 case "limit":
-                    console.log("LIMIT");
+                    this.limitAndOffset();
                     break;
                 case "offset":
-                    console.log("OFFSET");
+                    this.limitAndOffset();
                     break;
                 case "sort":
                     console.log("SORT");
                     const sort = this.params[key].split(',');
                     const sortField = this.capitalizeFirstLetter(sort[0]);
-
-                    this.list.sort(function (a, b) {
-                        const valA = a[sortField];
-                        const valB = b[sortField];
-                        function innerCompare(x, y) {
-                            if ((typeof x) === 'string')
-                                return x.toLowerCase().localeCompare(y.toLowerCase());
-                            else
-                                return compareNum(x, y);
-                        }
-                        function compareNum(x, y) {
-                            if (x === y) return 0;
-                            else if (x < y) return -1;
-                            return 1;
-                        }
-                        return innerCompare(valA, valB);
-                    })
-                    if (sort[1] == 'desc')
-                        this.list.reverse();
+                    this.sortList(sortField, sort);
                     break;
                 case "field":
-                    console.log("FIELD");
-                    let set = new Set();
+                    //console.log("FIELD");
                     let fields = this.params[key].split(',');
-                    const array = [];
-
+                    let tab = [];
 
                     for (let index = 0; index < this.list.length; index++) {
+                        let assoArray = [];
                         fields.forEach(field => {
-                            set.add(index);
-                            set[index].add(this.list[index][field]);
+                            assoArray[field] = this.list[index][field];
+
                         });
+                        tab.push(assoArray);
                     };
 
-                    set.forEach(s => array.push({t:s}));
+                    // Methode pour enlever les doublons : https://stackoverflow.com/q/2218999
+                    tab = tab.filter((value, index, self) =>
+                        index === self.findIndex((t) => (
+                                this.equal(t, value)
+                            ))
+                        );
+                    this.list = tab;
+                    
+                    // this.sortList(fields[0],'asc');
+                    // this.list = this.removeDuplicates(this.list, this.list.length);
+                    // this.list = tab;
 
-                    this.list = array;
+                    //console.log(tab);
+
                     break;
                 case "id":
                     let arr = [];
@@ -115,4 +108,55 @@ export default class CollectionFilter {
     capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
+    equal(ox, oy) {
+        let equal = true;
+        Object.keys(ox).forEach(function (member) {
+            if (ox[member] != oy[member]) {
+                equal = false;
+                return false;
+            }
+        })
+        return equal;
+    }
+    sortList(sortField, sort) {
+        this.list.sort(function (a, b) {
+            const valA = a[sortField];
+            const valB = b[sortField];
+            function innerCompare(x, y) {
+                if ((typeof x) === 'string')
+                    return x.toLowerCase().localeCompare(y.toLowerCase());
+                else
+                    return compareNum(x, y);
+            }
+            function compareNum(x, y) {
+                if (x === y) return 0;
+                else if (x < y) return -1;
+                return 1;
+            }
+            return innerCompare(valA, valB);
+        })
+        if (sort[1] == 'desc')
+            this.list.reverse();
+    }
+    limitAndOffset() {
+        //this.sortList("Id","asc");
+        if (this.params["limit"] == undefined || this.params["offset"] == undefined) {
+            return false;
+        } else {
+            //console.log(`${offset} here`);
+            let block = [];
+            let blockLength = parseInt(this.params["limit"]);
+            let idDebut = parseInt(blockLength * this.params["offset"]);
+            for (let index = idDebut; index < idDebut + blockLength; index++) {
+                const element = this.list[index];
+                block.push(element);
+            }
+            console.log(block);
+            this.list = block;
+            delete this.params["offset"];
+            delete this.params["limit"];
+            return;
+        }
+    }
+   
 }
